95196b247bd4341b5b528bb13c1c0398fa19c45e
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-01 01:28:54 +0000

    Added Divide, ShiftLeft, Xor and if>10

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -13,10 +13,26 @@ namespace SmoothOperator
             //converting string to byte
             byte con_num = byte.Parse(num);
 
-            //decreased result
+            //decrement result
             Console.WriteLine(--con_num);
-            //increased result
+            //increment result
             Console.WriteLine(++con_num);
+
+            //Divide by 2
+            Console.WriteLine(con_num/2);
+            //Shift left by 3
+            Console.WriteLine(con_num << 3);
+            //Xor with 5
+            Console.WriteLine(con_num^5);
+            //if > 10
+            if (con_num > 10)
+            {
+                Console.WriteLine(5 > 2);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
         }
     }
 }
