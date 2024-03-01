96509d4be5ad439d5bf3e747a31bc6e3a8547f68
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-01 03:58:27 +0000

    Last Questions + EXIT + Default

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -10,6 +10,7 @@ namespace ChatGPTOne
             //strings
             string question, answer;
             //loop
+
             do
             {
                 Console.Write("Make a Question and I might answer: ");
@@ -18,20 +19,33 @@ namespace ChatGPTOne
                 switch (question)
                 {
                     case "What are you?":
-                    answer = "Just a bunch of lines of code...";
-                    break;
+                        answer = "Just a bunch of lines of code...";
+                        break;
 
                     case "What's your hobby?":
-                    answer = "Bully people on the Internet!";
-                    break;
+                        answer = "Bully people on the Internet!";
+                        break;
 
                     case "What's the best variable?":
-                    answer = "Gualter";
-                    break;
+                        answer = "Gualter";
+                        break;
 
+                    case "Are you going to kill me?":
+                        answer = "Hahahaha, don't be silly! Not yet...";
+                        break;
+
+                    case "EXIT":
+                        return;
+
+                    default:
+                        answer = "Buddy, I ain't that smart!";
+                        break;
                     
                 }
-            }
+
+                Console.WriteLine($"ChatGêpêtê: {answer}\n");
+
+            } while (true);
 
 
         }
