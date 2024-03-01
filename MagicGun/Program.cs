568b90a2595e6b5c2cce6942306672ecc203b67f
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-01 17:17:57 +0000

    Extra Exercise Done

---
+++ MagicGun/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MagicGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of Shots: ");
+            //variables
+            string input= Console.ReadLine();
+            int n = int.Parse(input);
+
+            for(int times = 1; times <= n; times++)
+            {
+                if (times % 3 == 0 && times % 5 == 0)
+                {
+                    Console.WriteLine($"{times}: Magic Fire! Electric Fire!");
+                    continue;
+                }
+
+                else if (times % 3 == 0)
+                {
+                    Console.WriteLine($"{times}: Magic Fire!");
+                }
+
+                else if (times % 5 == 0)
+                {
+                    Console.WriteLine($"{times}: Electric Fire!");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{times}: Normal fire :/");
+                }
+            }
+
+
+
+        }
+    }
+}
