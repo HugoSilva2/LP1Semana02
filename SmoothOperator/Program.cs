9ff91519bfd3e35164df8d3f7c4e2528044c918e
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-01 01:12:06 +0000

    Increment and Decrement Number

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asking the input
+            Console.Write("Type an non negative integer: ");
+            string num = Console.ReadLine();
+
+            //converting string to byte
+            byte con_num = byte.Parse(num);
+
+            //decreased result
+            Console.WriteLine(--con_num);
+            //increased result
+            Console.WriteLine(++con_num);
+        }
+    }
+}
