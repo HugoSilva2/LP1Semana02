28ff3d36f8857a42541412b99bbb0635c1e6de0a
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-01 01:36:41 +0000

    Preparing Stage

---
+++ Cilindro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask Input of both height and radius
+
+            //Show Volume and Area
+        }
+    }
+}
