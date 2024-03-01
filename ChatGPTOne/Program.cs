86d4fe47c9c24524bde62015a69b623be5263f77
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-01 03:33:49 +0000

    Loop + First Question

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Formats.Asn1;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //strings
+            string question, answer;
+            //loop
+            do
+            {
+                Console.Write("Make a Question and I might answer: ");
+                question = Console.ReadLine();
+
+                switch (question)
+                {
+                    case "What are you?":
+                    answer = "Just a bunch of lines of code...";
+                    break;
+
+                }
+            }
+
+
+        }
+    }
+}
